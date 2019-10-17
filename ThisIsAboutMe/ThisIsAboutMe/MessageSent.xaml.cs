f429f651e7574d6315e1927f0772d25a3adf8650
0762634 <0762634@KELCE-ACIS10-P>
2019-10-17 01:39:19 -0500

    Finished

--- ThisIsAboutMe/ThisIsAboutMe/MessageSent.xaml.cs
+++ ThisIsAboutMe/ThisIsAboutMe/MessageSent.xaml.cs
@@ -16,5 +16,10 @@ namespace ThisIsAboutMe
         {
             InitializeComponent();
         }
+
+        private void Button_ClickedBack(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
     }
 }
