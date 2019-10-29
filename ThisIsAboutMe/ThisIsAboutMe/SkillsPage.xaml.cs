29602ee88fb26924954d6cf8d982f10bef1e3236
0762634 <0762634@KELCE-ACIS08-P>
2019-10-28 19:45:08 -0500

    Update13

--- ThisIsAboutMe/ThisIsAboutMe/SkillsPage.xaml.cs
+++ ThisIsAboutMe/ThisIsAboutMe/SkillsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,28 @@ namespace ThisIsAboutMe
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SkillsPage : ContentPage
+
+
     {
+        public ObservableCollection<string> ListingMySkills { get; set; }
+
+
+
         public SkillsPage()
         {
             InitializeComponent();
+
+            this.ListingMySkills = new ObservableCollection<string>();
+
+        }
+
+        private void InputButton_Clicked(object sender, EventArgs e)
+        {
+            this.ListingMySkills.Add(InputAskill.Text);
+
+            InputAskill.Text = "";
+
+            SkillsListView.ItemsSource = this.ListingMySkills;
         }
     }
 }
